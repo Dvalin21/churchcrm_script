7454851ab88438312dbbdcd6a2380982f17ba386
Dvalin21 <dvalin21@gmail.com>
2024-06-26 00:30:19 -0500

    Update setup_churchcrm.sh

--- setup_churchcrm.sh
+++ setup_churchcrm.sh
@@ -110,7 +110,7 @@ echo "Fail2Ban installation and configuration completed successfully."
 echo "Running mysql_secure_installation..."
 
 # Prompt user for MySQL root password
-#prompt_for_password
+prompt_for_password
 
 # Here we use a heredoc to provide input to the mysql_secure_installation script non-interactively
 sudo mysql_secure_installation <<EOF
@@ -182,15 +182,25 @@ sudo unzip ChurchCRM-latest.zip -d /var/www/
 sudo chown -R www-data:www-data /var/www/churchcrm/
 sudo chmod -R 755 /var/www/churchcrm/
 
-# Generate a 64-character 2FA secret key
-two_fa_secret=$(tr -dc '[:alnum:]!@#$%^&*()_+-=[]{}|;:,.<>?' < /dev/urandom | head -c 64)
 
 # Update Config.php with database credentials and 2FA secret key
 config_file="/var/www/churchcrm/Include/Config.php.example"
 sudo sed -i "s/\$sUSER = '.*';/\$sUSER = 'churchcrmuser';/" "$config_file"
 sudo sed -i "s/\$sPASSWORD = '.*';/\$sPASSWORD = '$db_user_password';/" "$config_file"
 sudo sed -i "s/\$sDATABASE = '.*';/\$sDATABASE = 'churchcrm';/" "$config_file"
-sudo sed -i "#/\#$TwoFASecretKey = '.*';/\$TwoFASecretKey = '$two_fa_secret';/" "$config_file"
+
+# Update Config.php with 2FA secret key
+two_fa_secret=$(tr -dc '[:alnum:]!@#$%^&*()_+-=[]{}|;:,.<>?' < /dev/urandom | head -c 64)
+
+# Update Config.php with database credentials and 2FA secret key
+config_file="/var/www/churchcrm/Include/Config.php.example"
+
+# Remove the # from the line containing TwoFASecretKey
+sudo sed -i 's/^#\( *\$TwoFASecretKey = .*\)$/\1/' "$config_file"
+
+# Use sed to update the key
+sudo sed -i "s/\$TwoFASecretKey = '.*';/\$TwoFASecretKey = '$two_fa_secret';/" "$config_file"
+
 
 # Prompt user for Apache configuration details
 read -p "Enter ServerAdmin email (e.g., admin@example.com): " server_admin
